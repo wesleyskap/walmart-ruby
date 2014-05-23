660e062ef3eb956693b5ac33471f857a0be73548
Diego Carrion <dc.rec1@gmail.com>
2014-05-23 17:52:04 -0300

    items can be find by origin sku

--- lib/extra/item.rb
+++ lib/extra/item.rb
@@ -1,7 +1,7 @@
 module Extra
   class Item < Base
     def self.find(sku_id)
-      parse_get "sellerItems/#{sku_id}"
+      parse_get("sellerItems/#{sku_id.is_a?(Hash) ? "skuOrigin/#{sku_id['origin_sku']}" : sku_id}")
     end
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,9 +2,15 @@ require 'spec_helper'
 
 describe Extra::Item do
   describe ".find" do
-    it "find an item by the sku id" do
+    it "find an item by the Extra sku id" do
       VCR.use_cassette('items_find') do
         Extra::Item.find('3454385')['skuOrigin'].should == '219802'
+      end
+    end
+
+    it "find an item by the origin sku id" do
+      VCR.use_cassette('items_find_by_origin_sku') do
+        Extra::Item.find('origin_sku' => '219802')['skuId'].should == '3454385'
       end
     end
   end
