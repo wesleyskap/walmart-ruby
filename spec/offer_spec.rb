93107f2fba6dadf93a50259dc9fa601ba1ddb075
Diego Carrion <dc.rec1@gmail.com>
2014-06-18 20:29:24 -0300

    removed rspec deprecations

--- spec/offer_spec.rb
+++ spec/offer_spec.rb
@@ -6,14 +6,14 @@ describe Walmart::Offer do
       offer = VCR.use_cassette('offer_success') do
         Walmart::Offer.create! id: 1234, name: 'Walmart Ruby', description: 'Walmart Ruby gem', active: true, ean: '1234567890123', sellerSKU: '1234567', height: 10, width: 5, weight: 1, price: 12.45, priceDiscount: 10, brand: 'i-Supply', urlImage: "http://images.isupply.com.br/1234567"
       end
-      offer.should be_persisted
+      expect(offer).to be_persisted
     end
 
     it "should not persist an invalid offer" do
       offer = VCR.use_cassette('offer_not_success') do
         Walmart::Offer.create! id: 'not-long'
       end
-      offer.should_not be_persisted
+      expect(offer).to_not be_persisted
     end
   end
 
@@ -21,7 +21,7 @@ describe Walmart::Offer do
     it "should update an offer price" do
       offer = Walmart::Offer.new(sku: '1234567')
       VCR.use_cassette('offer_price_success') do
-        offer.update_price!(15.00, listPrice: 20.00).should be_true
+        expect(offer.update_price!(15.00, listPrice: 20.00)).to be_truthy
       end
     end
   end
@@ -30,7 +30,7 @@ describe Walmart::Offer do
     it "should update an offer quantity" do
       offer = Walmart::Offer.new(sku: '1234567')
       VCR.use_cassette('offer_quantity_success') do
-        offer.update_quantity!(4).should be_true
+        expect(offer.update_quantity!(4)).to be_truthy
       end
     end
   end
